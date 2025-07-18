453dd6d91f5ff0869d1e46babcb675cde4504612
iwanhkim <ikhwanulhkim@gmail.com>
2025-07-18 18:14:38 +0800

    install cypress

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000', // default
+    chromeWebSecurity: false,
+    experimentalModifyObstructiveThirdPartyCode: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
